c6ef46df6f01d1f12c56d1ae09a15d098d1c3304
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-26 19:29:25 +0300

    file updated

--- src/component/Nav/NavBar.jsx
+++ src/component/Nav/NavBar.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import './NavBar.css';
+import RightNav from './RightNav';
 function NavBar() {
     return (
         <div className="navbar_container">
             <div className="logo">
                 <h2>Education <span>Site</span></h2>
             </div>
-            <ul>
-                <li>Home</li>
-                <li>Courses</li>
-                <li>About Us</li>
-                <li>Pages</li>
-                <li>Events</li>
-                <li>Blog</li>
-                <li>Contact</li>
-
-            </ul>
+            <RightNav/>
             
         </div>
     )
