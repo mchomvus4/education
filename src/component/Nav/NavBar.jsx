71645abb31dc1574efc7f268ea20ca75c9a4db47
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-26 19:09:55 +0300

    file updated

--- src/component/Nav/NavBar.jsx
+++ src/component/Nav/NavBar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import './NavBar.css';
 function NavBar() {
     return (
-        <div>
-        Education
+        <div className="navbar_container">
+            <div className="logo">
+                <h2>Education <span>Site</span></h2>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Courses</li>
+                <li>About Us</li>
+                <li>Pages</li>
+                <li>Events</li>
+                <li>Blog</li>
+                <li>Contact</li>
+
+            </ul>
+            
         </div>
     )
 }
