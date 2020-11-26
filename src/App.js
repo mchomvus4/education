c107e62918fd565c36a0db34b617533bb7bb151e
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-26 18:01:54 +0300

    Nav bar file updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import NavBar from './component/Nav/NavBar';
 
 
 function App() {
   return (
     <div className="">
-       Education
+       <NavBar/>
     </div>
   );
 }
---
+++ src/component/Nav/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './NavBar.css';
+function NavBar() {
+    return (
+        <div className="navbar_container">
+            <div className="logo">
+                <h2>Education <span>Site</span></h2>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Courses</li>
+                <li>About Us</li>
+                <li>Pages</li>
+                <li>Events</li>
+                <li>Blog</li>
+                <li>Contacts</li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default NavBar
