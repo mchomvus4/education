e97e682538a7f8f9330f70af0cd7fb9fdff901ee
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-30 13:22:19 +0300

    files updated

--- src/components/Section/About.jsx
+++ src/components/Section/About.jsx
@@ -10,19 +10,24 @@ function About() {
         <section>
             <div className="flex_about">
                 <div>
-                    <img src={earth} alt="about_img"/>
+                    <img src={earth} alt="about_img" />
+                    <p className="about_para">Our Program</p>
                 </div>
                 <div>
-                    <img src={university} alt="about_img"/>
+                    <img src={university} alt="about_img" />
+                    <p className="about_para">Our History</p>
                 </div>
                 <div>
-                    <img src={book} alt="about_img"/>
+                    <img src={book} alt="about_img" />
+                   <p className="about_para"> Research</p>
                 </div>
                 <div>
-                    <img src={student} alt="about_img"/>
+                    <img src={student} alt="about_img" />
+                   <p className="about_para"> Student</p>
                 </div>
                 <div>
-                    <img src={diploma} alt="about_img"/>
+                    <img src={diploma} alt="about_img" />
+                    <p className="about_para">High Quality</p>
                 </div>
             </div>
        </section>
