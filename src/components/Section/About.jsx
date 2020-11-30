f42dc5bd5b12031eb21860967fe9d471d696573d
samwelnm4@gmail.com <samwelnm4@gmail.com>
2020-11-30 13:12:57 +0300

    files updated

--- src/components/Section/About.jsx
+++ src/components/Section/About.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-
+import './About.css';
+import earth from '../../images/earth-globe.svg';
+import university from '../../images/university.svg';
+import book from '../../images/books.svg';
+import student from '../../images/student.svg';
+import diploma from '../../images/diploma.svg';
 function About() {
     return (
-        <div>
-            <h2>About Us Component</h2>
+        <section>
+            <div className="flex_about">
+                <div>
+                    <img src={earth} alt="about_img"/>
+                </div>
+                <div>
+                    <img src={university} alt="about_img"/>
+                </div>
+                <div>
+                    <img src={book} alt="about_img"/>
+                </div>
+                <div>
+                    <img src={student} alt="about_img"/>
+                </div>
+                <div>
+                    <img src={diploma} alt="about_img"/>
+                </div>
             </div>
+       </section>
     )
 }
 
